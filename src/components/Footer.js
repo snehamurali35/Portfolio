bda2bacbd47630ddf1820b553b0d6edeee1380e6
Sneha <snehamur@cs.washington.edu>
2026-05-19 15:02:37 -0700

    Fix Vercel build warnings 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,6 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
