f67c48f4846ba3d81272025752eb4f18f60d69d0
Sneha <snehamur@cs.washington.edu>
2026-05-19 16:50:16 -0700

    new resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://portfolio-svc6-delta.vercel.app"
                 target="_blank"
                 rel="noreferrer"
               >
